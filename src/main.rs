e000d27befb2daab6f5e7fd28c4af4b0a3ae18eb
Andrés Villalobos <andresalvivar@gmail.com>
2022-08-29 01:55:39 -0600

    Only one more test to complete the set, the ubsubscription.

--- src/main.rs
+++ src/main.rs
@@ -259,11 +259,11 @@ mod bite_tests {
 
         let id = get_id(&mut conn);
 
-        conn.try_write(stamp_header(b"s kv.1 ONE".to_vec(), id, 0))
+        conn.try_write(stamp_header(b"s kv.1 1".to_vec(), id, 0))
             .unwrap();
-        conn.try_write(stamp_header(b"s kv.1.2 TWO".to_vec(), id, 0))
+        conn.try_write(stamp_header(b"s kv.1.2 1.2".to_vec(), id, 0))
             .unwrap();
-        conn.try_write(stamp_header(b"s kv.1.2.3 THREE".to_vec(), id, 0))
+        conn.try_write(stamp_header(b"s kv.1.2.3 1.2.3".to_vec(), id, 0))
             .unwrap();
         conn.try_write(stamp_header(b"k kv".to_vec(), id, 0))
             .unwrap();
@@ -274,8 +274,8 @@ mod bite_tests {
             response,
             &[
                 0, id as u8, 0, 0, 0, 8, 79, 75, 0, id as u8, 0, 0, 0, 8, 79, 75, 0, id as u8, 0,
-                0, 0, 8, 79, 75, 0, id as u8, 0, 0, 0, 25, 49, 32, 79, 78, 69, 0, 50, 32, 84, 87,
-                79, 0, 51, 32, 84, 72, 82, 69, 69
+                0, 0, 8, 79, 75, 0, id as u8, 0, 0, 0, 23, 49, 32, 49, 0, 50, 32, 49, 46, 50, 0,
+                51, 32, 49, 46, 50, 46, 51
             ]
         );
     }
@@ -360,83 +360,59 @@ mod bite_tests {
         );
     }
 
-    // #[test]
-    // fn subscriptions() {
-    //     let server = TcpStream::connect("127.0.0.1:1984").unwrap();
-    //     server.set_nonblocking(true).unwrap();
+    #[test]
+    fn subscriptions() {
+        let server = TcpStream::connect("127.0.0.1:1984").unwrap();
+        server.set_nonblocking(true).unwrap();
 
-    //     let addr = server.local_addr().unwrap();
-    //     let mut conn = Connection::new(0, server, addr);
+        let addr = server.local_addr().unwrap();
+        let mut conn = Connection::new(0, server, addr);
 
-    //     // This test fails when key_value has more children than expected. We
-    //     // are assuming an empty database.
+        // This test fails when key_value has more children than expected. We
+        // are assuming an empty database.
 
-    //     conn.try_write(b"#g subs".to_vec()).unwrap();
-    //     conn.try_write(b"#j subs".to_vec()).unwrap();
-    //     conn.try_write(b"#k subs".to_vec()).unwrap();
+        let id = get_id(&mut conn);
 
-    //     conn.try_write(b"s subs.user User".to_vec())
-    //         .unwrap();
-    //     conn.try_write(b"d subs.city".to_vec()).unwrap();
-    //     conn.try_write(b"+ subs.city City".to_vec())
-    //         .unwrap();
-    //     conn.try_write(b"d subs.age".to_vec()).unwrap();
-    //     conn.try_write(b"+1 subs.age Age".to_vec()).unwrap();
-    //     conn.try_write(b"d subs.user.id".to_vec()).unwrap();
-    //     conn.try_write(b"+1 subs.user.id User.ID".to_vec())
-    //         .unwrap();
-    //     conn.try_write(b"! subs.user.name User.Name".to_vec())
-    //         .unwrap();
-    //     conn.try_write(b"! subs.user.name.first User.Name.First".to_vec())
-    //         .unwrap();
-    //     conn.try_write(b"! subs.user.name.last User.Name.Last".to_vec())
-    //         .unwrap();
+        conn.try_write(stamp_header(b"#g subs".to_vec(), id, 0))
+            .unwrap();
+        conn.try_write(stamp_header(b"#k subs".to_vec(), id, 0))
+            .unwrap();
+        conn.try_write(stamp_header(b"#j subs".to_vec(), id, 0))
+            .unwrap();
 
-    //     conn.try_write(b"#- subs".to_vec()).unwrap();
+        conn.try_write(stamp_header(b"s subs SET".to_vec(), id, 0))
+            .unwrap();
+        conn.try_write(stamp_header(b"+1 subs".to_vec(), id, 0))
+            .unwrap();
+        conn.try_write(stamp_header(b"+ subs +".to_vec(), id, 0))
+            .unwrap();
+        conn.try_write(stamp_header(b"! subs CALL".to_vec(), id, 0))
+            .unwrap();
 
-    //     conn.try_write(b"s subs.user User".to_vec())
-    //         .unwrap();
-    //     conn.try_write(b"d subs.city".to_vec()).unwrap();
-    //     conn.try_write(b"+ subs.city City".to_vec())
-    //         .unwrap();
-    //     conn.try_write(b"d subs.age".to_vec()).unwrap();
-    //     conn.try_write(b"+1 subs.age Age".to_vec()).unwrap();
+        let response = get_read(&mut conn);
 
-    //     sleep(Duration::from_millis(200));
-    //     let response = conn.try_read().unwrap();
-    //     println!("{:?}\n", response);
-    //     println!("{}", String::from_utf8_lossy(&response));
-    //     sleep(Duration::from_millis(200));
-
-    //     assert_eq!(
-    //         response,
-    //         &[
-    //             79, 75, 10, 79, 75, 10, 79, 75, 10, 79, 75, 10, 85, 115, 101, 114, 10, 123, 34,
-    //             117, 115, 101, 114, 34, 58, 34, 85, 115, 101, 114, 34, 125, 10, 117, 115, 101, 114,
-    //             32, 85, 115, 101, 114, 10, 79, 75, 10, 67, 105, 116, 121, 10, 67, 105, 116, 121,
-    //             10, 123, 34, 99, 105, 116, 121, 34, 58, 34, 67, 105, 116, 121, 34, 125, 10, 99,
-    //             105, 116, 121, 32, 67, 105, 116, 121, 10, 79, 75, 10, 0, 0, 0, 0, 0, 0, 0, 0, 10,
-    //             0, 0, 0, 0, 0, 0, 0, 0, 10, 123, 34, 97, 103, 101, 34, 58, 34, 92, 117, 48, 48, 48,
-    //             48, 92, 117, 48, 48, 48, 48, 92, 117, 48, 48, 48, 48, 92, 117, 48, 48, 48, 48, 92,
-    //             117, 48, 48, 48, 48, 92, 117, 48, 48, 48, 48, 92, 117, 48, 48, 48, 48, 92, 117, 48,
-    //             48, 48, 48, 34, 125, 10, 97, 103, 101, 32, 0, 0, 0, 0, 0, 0, 0, 0, 10, 79, 75, 10,
-    //             0, 0, 0, 0, 0, 0, 0, 0, 10, 0, 0, 0, 0, 0, 0, 0, 0, 10, 123, 34, 105, 100, 34, 58,
-    //             34, 92, 117, 48, 48, 48, 48, 92, 117, 48, 48, 48, 48, 92, 117, 48, 48, 48, 48, 92,
-    //             117, 48, 48, 48, 48, 92, 117, 48, 48, 48, 48, 92, 117, 48, 48, 48, 48, 92, 117, 48,
-    //             48, 48, 48, 92, 117, 48, 48, 48, 48, 34, 125, 10, 105, 100, 32, 0, 0, 0, 0, 0, 0,
-    //             0, 0, 10, 79, 75, 10, 85, 115, 101, 114, 46, 78, 97, 109, 101, 10, 123, 34, 110,
-    //             97, 109, 101, 34, 58, 34, 85, 115, 101, 114, 46, 78, 97, 109, 101, 34, 125, 10,
-    //             110, 97, 109, 101, 32, 85, 115, 101, 114, 46, 78, 97, 109, 101, 10, 79, 75, 10, 85,
-    //             115, 101, 114, 46, 78, 97, 109, 101, 46, 70, 105, 114, 115, 116, 10, 123, 34, 102,
-    //             105, 114, 115, 116, 34, 58, 34, 85, 115, 101, 114, 46, 78, 97, 109, 101, 46, 70,
-    //             105, 114, 115, 116, 34, 125, 10, 102, 105, 114, 115, 116, 32, 85, 115, 101, 114,
-    //             46, 78, 97, 109, 101, 46, 70, 105, 114, 115, 116, 10, 79, 75, 10, 85, 115, 101,
-    //             114, 46, 78, 97, 109, 101, 46, 76, 97, 115, 116, 10, 123, 34, 108, 97, 115, 116,
-    //             34, 58, 34, 85, 115, 101, 114, 46, 78, 97, 109, 101, 46, 76, 97, 115, 116, 34, 125,
-    //             10, 108, 97, 115, 116, 32, 85, 115, 101, 114, 46, 78, 97, 109, 101, 46, 76, 97,
-    //             115, 116, 10, 79, 75, 10, 79, 75, 10, 79, 75, 10, 67, 105, 116, 121, 10, 79, 75,
-    //             10, 0, 0, 0, 0, 0, 0, 0, 0, 10
-    //         ]
-    //     );
-    // }
+        assert_eq!(
+            response,
+            &[
+                0, id as u8, 0, 0, 0, 8, 79, 75, 0, id as u8, 0, 0, 0, 8, 79, 75, 0, id as u8, 0,
+                0, 0, 8, 79, 75, 0, id as u8, 0, 0, 0, 8, 79, 75, 0, id as u8, 0, 0, 0, 9, 83, 69,
+                84, 0, id as u8, 0, 0, 0, 14, 115, 117, 98, 115, 32, 83, 69, 84, 0, id as u8, 0, 0,
+                0, 20, 123, 34, 115, 117, 98, 115, 34, 58, 34, 83, 69, 84, 34, 125, 0, id as u8, 0,
+                0, 0, 14, 0, 0, 0, 0, 0, 0, 0, 1, 0, id as u8, 0, 0, 0, 14, 0, 0, 0, 0, 0, 0, 0, 1,
+                0, id as u8, 0, 0, 0, 19, 115, 117, 98, 115, 32, 0, 0, 0, 0, 0, 0, 0, 1, 0,
+                id as u8, 0, 0, 0, 65, 123, 34, 115, 117, 98, 115, 34, 58, 34, 92, 117, 48, 48, 48,
+                48, 92, 117, 48, 48, 48, 48, 92, 117, 48, 48, 48, 48, 92, 117, 48, 48, 48, 48, 92,
+                117, 48, 48, 48, 48, 92, 117, 48, 48, 48, 48, 92, 117, 48, 48, 48, 48, 92, 117, 48,
+                48, 48, 49, 34, 125, 0, id as u8, 0, 0, 0, 15, 0, 0, 0, 0, 0, 0, 0, 1, 43, 0,
+                id as u8, 0, 0, 0, 15, 0, 0, 0, 0, 0, 0, 0, 1, 43, 0, id as u8, 0, 0, 0, 20, 115,
+                117, 98, 115, 32, 0, 0, 0, 0, 0, 0, 0, 1, 43, 0, id as u8, 0, 0, 0, 66, 123, 34,
+                115, 117, 98, 115, 34, 58, 34, 92, 117, 48, 48, 48, 48, 92, 117, 48, 48, 48, 48,
+                92, 117, 48, 48, 48, 48, 92, 117, 48, 48, 48, 48, 92, 117, 48, 48, 48, 48, 92, 117,
+                48, 48, 48, 48, 92, 117, 48, 48, 48, 48, 92, 117, 48, 48, 48, 49, 43, 34, 125, 0,
+                id as u8, 0, 0, 0, 8, 79, 75, 0, id as u8, 0, 0, 0, 10, 67, 65, 76, 76, 0,
+                id as u8, 0, 0, 0, 15, 115, 117, 98, 115, 32, 67, 65, 76, 76, 0, id as u8, 0, 0, 0,
+                21, 123, 34, 115, 117, 98, 115, 34, 58, 34, 67, 65, 76, 76, 34, 125
+            ]
+        );
+    }
 }
